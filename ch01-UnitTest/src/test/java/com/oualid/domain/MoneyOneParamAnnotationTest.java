568c177afbe442f2c6ccffd3df236ca63a92e1cd
amisrhir oualid <amisrhiroualid94@gmail.com>
2025-07-07 23:17:52 +0100

    More than one parameter using @Parameterized + @CsvSource

--- ch01-UnitTest/src/test/java/com/oualid/domain/MoneyOneParamAnnotationTest.java
+++ ch01-UnitTest/src/test/java/com/oualid/domain/MoneyOneParamAnnotationTest.java
@@ -1,16 +1,30 @@
 package com.oualid.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoneyOneParamAnnotationTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {10,15,90})
+    @ValueSource(ints = {10,15,90})//single param
     void constructorShouldSetAmountAndCurrency(int amount){
         Money money = new Money(amount,"USD");
+        assertThat(money.getAmount()).isEqualTo(amount);
+    }
 
-        Assertions.assertThat(money.getAmount()).isEqualTo(amount);
+    @ParameterizedTest
+    @CsvSource({
+            "10, USD",
+            "15, DH",
+            "90, EUR"
+    })
+    void constructorShouldSetAmountAndCurrency(int amount,String concurrency){
+        Money money = new Money(amount,concurrency);
+
+        assertThat(money.getAmount()).isEqualTo(amount);
+        assertThat(money.getCurrency()).isEqualTo(concurrency);
     }
 }
