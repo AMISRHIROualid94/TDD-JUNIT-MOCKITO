feddf5ce5c29c5b19631726cdea3e3fe2d472e87
amisrhir oualid <amisrhiroualid94@gmail.com>
2025-09-28 19:35:19 +0100

    test phases

--- ch01-UnitTest/src/test/java/com/oualid/domain/ClientTest.java
+++ ch01-UnitTest/src/test/java/com/oualid/domain/ClientTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 public class ClientTest {
+    /* Arrange phase (required objects)*/
     private Address firstAddress = new Address("address 1");
     private Address secondAddress = new Address("address 2");
     private final int EMPTY_LIST_SIZE = 0;
     private final int UNIQUE_ELEMENT_SIZE = 1;
     private Client newClient;
     private int addressSize = 0;
+    /*-----------------------------*/
+
 
     @BeforeEach
     void setUps(){
@@ -24,7 +27,9 @@ public class ClientTest {
     @Test
     void afterCreationShouldHaveNoAddress(){
         addressSize = newClient.getAddressesSize();
+        /* Assert phase (verify the results) */
         Assertions.assertThat(addressSize).isEqualTo(EMPTY_LIST_SIZE);
+        /*-------------------------*/
         System.out.println("afterCreationShouldHaveNoAddress test");
     }
 
@@ -32,14 +37,20 @@ public class ClientTest {
     void shouldAllowToAddAddress(){
         newClient.getAddresses().add(firstAddress);
         addressSize = newClient.getAddressesSize();
+        /* Assert phase (verify the results) */
         Assertions.assertThat(addressSize).isEqualTo(UNIQUE_ELEMENT_SIZE);
+        /*-------------------------*/
         System.out.println("shouldAllowToAddAddress test");
     }
     @Test
     void shouldAllowToAddManyAddresses(){
+        /* Act phase (execution of SUT method(s) to be tested) */
         newClient.getAddresses().addAll(Arrays.asList(firstAddress,secondAddress));
+        /*-------------*/
         addressSize = newClient.getAddressesSize();
+        /* Assert phase (verify the results) */
         Assertions.assertThat(addressSize).isGreaterThan(UNIQUE_ELEMENT_SIZE);
+        /*-------------------------*/
         System.out.println("shouldAllowToAddManyAddresses test");
     }
 }
